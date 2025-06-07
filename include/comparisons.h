7e3dde7ad9ec03f022578a762201601913c324e9
praetortuicu <teodor.ticu@outlook.com>
2025-06-08 00:51:38 +0200

    Add more comparison functions Modify TODO and CHECKPOINTS

--- include/comparisons.h
+++ include/comparisons.h
@@ -2,7 +2,7 @@
  *	List of comparison macros necessary for unit testing.
  *	Date	07/06/25
  *	Author	Teodor	Ticu
- *	Version	0.0.2
+ *	Version	0.0.3
  *
  */
 
@@ -39,6 +39,62 @@ if	(value1	!=	value2)	{\
 }\
 
 
+
+/*
+ *	@param	value1	Value to check
+ *	@param	value2	Other value to check
+ *
+ * 	@brief	Less than check on two values.
+ */
+#define	CHECK_LESS_THAN(value1,	value2)	\
+if	(value1	<	value2)	{\
+	printf("Less than test passed. Exit code: %d\n", ERROR_CODE_TRUE);	\
+}	else	{\
+	printf("Less than test failed. Exit code: %d\n", ERROR_CODE_FALSE);	\
+}\
+
+
+/*
+ *	@param	value1	Value to check
+ *	@param	value2	Other value to check
+ *
+ * 	@brief	Greater than check on two values.
+ */
+#define	CHECK_GREATER_THAN(value1,	value2)	\
+if	(value1	>	value2)	{\
+	printf("Greater than test passed. Exit code: %d\n", ERROR_CODE_TRUE);	\
+}	else	{\
+	printf("Greater than test failed. Exit code: %d\n", ERROR_CODE_FALSE);	\
+}\
+
+/**
+ *	@param	value	Value to check
+ *	@param	min		Lower bound
+ *	@param	max		Upper bound
+ *
+ *	@brief	Checks whether value is between min and max
+ */
+#define	CHECK_IN_RANGE(value,	min,	max)	\
+if	(value	<=	max	&&	value	>=	min)	{	\
+	printf("In range test passed. Exit code: %d\n", ERROR_CODE_TRUE);	\
+}	else	{\
+	printf("In range test failed. Exit code: %d\n", ERROR_CODE_FALSE);	\
+}\
+
+
+/**
+ *	@param	value	Value to check
+ *	@param	min		Lower bound
+ *	@param	max		Upper bound
+ *
+ *	@brief	Checks whether value is not between min and max
+ */
+#define	CHECK_NOT_IN_RANGE(value,	min,	max)	\
+if	(value	>=	max	||	value	<=	min)	{	\
+	printf("Not in range test passed. Exit code: %d\n", ERROR_CODE_TRUE);	\
+}	else	{\
+	printf("Not in range test failed. Exit code: %d\n", ERROR_CODE_FALSE);	\
+}\
 
 
 
